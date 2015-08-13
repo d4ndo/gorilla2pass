8df9ec4a76cbb2e52ddcfd1b90b857511d5c5f39
pcre <pcre@gmx.de>
2015-08-13 15:00:22 +0200

    Added some config flags to change the organistaion structure.

--- gorilla2pass.sh
+++ gorilla2pass.sh
@@ -3,8 +3,12 @@
 # This file is licensed under the GPLv3+. Please see LICENSE for more information.
 
 gorillacontainer="$1"
-#Append Title to Group
+#Append Title to Group path
 appendTitle="TRUE"
+#Append URL to multiline password
+multilineURL="FALSE"
+#Append notes to multiline password
+multilineNOTES="TRUE"
 
 #"uuid,group,title,url,user,password,notes"
 cat "$gorillacontainer" | sed -n '1!p' | while IFS="," read -r uuid group title url user password notes; do
@@ -12,7 +16,8 @@ cat "$gorillacontainer" | sed -n '1!p' | while IFS="," read -r uuid group title 
 	group="$(sed -e 's/\\\./#/g' -e 's/\./\//g' -e 's/#/./g' <<< "$group")"
 	title="$(sed -e 's/$/\//' <<< "$title")"
 
-	test "$notes" != "" && password="$password\n$notes"
+	if [[ $url != "" && $multilineURL == "TRUE" ]]; then password="$password\n$url"; fi
+	if [[ $notes != "" && $multilineNOTES == "TRUE" ]]; then password="$password\n$notes"; fi
 	entry="$group/$user"
 	test "$appendTitle" = "TRUE" && entry="$group/$title/$user"
 
