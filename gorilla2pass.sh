1d3b047b95eb5ebf61a95574bb655abef5b422be
d4ndo <daniel@luke>
2015-08-15 00:59:45 +0200

    Fixed whitespace in title. Added entryURL to get additional url to user.

--- gorilla2pass.sh
+++ gorilla2pass.sh
@@ -9,18 +9,28 @@ appendTitle="TRUE"
 multilineURL="FALSE"
 #Append notes to multiline password
 multilineNOTES="TRUE"
+#Add an own entry url
+entryURL="FALSE"
 
 #"uuid,group,title,url,user,password,notes"
 cat "$gorillacontainer" | sed -n '1!p' | while IFS="," read -r uuid group title url user password notes; do
 
 	group="$(sed -e 's/\\\./#/g' -e 's/\./\//g' -e 's/#/./g' <<< "$group")"
-	title="$(sed -e 's/$/\//' <<< "$title")"
+        title="$(sed -s 's/\s\{1,\}/_/g' <<< "$title")"
 
 	if [[ $url != "" && $multilineURL == "TRUE" ]]; then password="$password\n$url"; fi
 	if [[ $notes != "" && $multilineNOTES == "TRUE" ]]; then password="$password\n$notes"; fi
-	entry="$group/$user"
-	test "$appendTitle" = "TRUE" && entry="$group/$title/$user"
 
+	entry="$group/$user"
+        if [[ $title != "" && $appendTitle == "TRUE" ]]; then
+            password="$password\n$notes"
+	    entry="$group/$title/$user"
+        fi
 	echo -e "$password" | pass insert --multiline --force $entry
 	test $? && echo "Added! $entry"
+	if [[ $url != "" && $entryURL == "TRUE" ]]; then
+	    entry="$group/$title/url"
+	    echo -e "$url" | pass insert --multiline --force $entry
+            test $? && echo "Added! $entry"
+        fi
 done
