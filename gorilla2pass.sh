b534781ced3f390b62cc256d702182cae0dadf5d
Daniel Dörrhöfer <pcre@gmx.de>
2016-08-07 11:08:35 +0200

    gorilla2pass now supports two differnt kind of export formats. It detects if export was created from a "debian password gorilla".

--- gorilla2pass.sh
+++ gorilla2pass.sh
@@ -5,15 +5,36 @@
 gorillacontainer="$1"
 #User is used as leaf containting the password
 leafUser="TRUE"
-#Append URL to multiline password
+#Append URL to multiline password (Debian universe only)
 multilineURL="FALSE"
 #Append notes to multiline password
 multilineNOTES="TRUE"
 #Add an own entry url
 entryURL="FALSE"
 
-#"uuid,group,title,url,user,password,notes"
-cat "$gorillacontainer" | sed -n '1!p' | while IFS="," read -r uuid group title url user password notes; do
+### Test the password gorilla export file ### 
+# Password gorilla maintained by debian adds a header to the export file,
+# because it also supports an import function. see (pass2gorilla).
+# It also includes the url.
+# There are two version of password gorilla export files:
+# The debian universe and the rest of the unix universe.
+yes="1!p"
+no="p"
+debianreg="uuid,group,title,url,user,password"
+debian="uuid group title url user password notes"
+rest="uuid group title user password notes"
+
+#By default assume non Debian export file. Do not snip away the header.
+snip="$no";
+parse="$rest";
+#Detect if debian header is set.
+if egrep -q "$debianreg" <<< head -n1 "$gorillacontainer"; then
+        snip="$yes";
+        parse="$debian";
+fi
+### end of test ###
+
+cat "$gorillacontainer" | sed -n "$snip" | while IFS="," read -r $parse; do
 
 	group="$(sed -e 's/\\\./#/g' -e 's/\./\//g' -e 's/#/./g' <<< "$group")"
         title="$(sed -s 's/\s\{1,\}/_/g' <<< "$title")"
